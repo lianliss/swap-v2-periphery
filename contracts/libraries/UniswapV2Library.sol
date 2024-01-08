0450f0541196bfc2c6f70d17762bacc132c06a93
lianliss <ldevil2209@ya.ru>
2024-01-09 00:21:54 +0800

    Add pixel init hash

--- contracts/libraries/UniswapV2Library.sol
+++ contracts/libraries/UniswapV2Library.sol
@@ -21,7 +21,7 @@ library UniswapV2Library {
                 hex'ff',
                 factory,
                 keccak256(abi.encodePacked(token0, token1)),
-                hex'96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f' // init code hash
+                hex'0968bfcf8adae8e1d3231abcf029e4786ce22a36909941f9b2b34daf79e193c4' // init code hash
             ))));
     }
 
